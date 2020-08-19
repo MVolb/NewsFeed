0937aaa9167c83339b4c5850c6b579a9c538e6d5
Martin <martin.volb@gmx.at>
2020-08-19 10:56:06 +0200

    changed layout for fragment_weather

--- app/src/main/java/com/martin/volb/newsapp/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/martin/volb/newsapp/ui/weather/WeatherFragment.java
@@ -11,15 +11,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.martin.volb.newsapp.R;
@@ -34,7 +30,7 @@ public class WeatherFragment extends Fragment implements WeatherView, LocationLi
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_weather, container, false);
-        currentWeatherAnimationView = root.findViewById(R.id.current_weather_animation_view);
+        currentWeatherAnimationView = root.findViewById(R.id.current_weather_animation_view_current);
         presenter = new WeatherPresenter(this);
         locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
         if (isLocationPermissionEnabled()) {
